a1552c929f6f07e5ce17f046a92db91a8b237d94
Dmitry Baranouski <bardmitry@bk.ru>
2023-02-02 12:50:07 +0300

    Task_10

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -3,3 +3,9 @@
 // 456 -> 5
 // 782 -> 8
 // 918 -> 1
+
+Console.WriteLine("Введите трехзначное число: ");
+int A = int.Parse(Console.ReadLine()!);
+int B = A % 100;
+int C = B / 10;
+Console.WriteLine(C);
