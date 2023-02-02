9c9601dd33bcb4d48113c69cf61d0d77c3048f79
Dmitry Baranouski <bardmitry@bk.ru>
2023-02-02 14:09:34 +0300

    итог

--- Task_15/Program.cs
+++ Task_15/Program.cs
@@ -3,3 +3,15 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+Console.WriteLine("вводим число от 1 до 7:");
+int Day=int.Parse(Console.ReadLine()!);
+
+if(Day==6 || Day==7)
+{
+    Console.WriteLine($"Yes!");
+}
+else
+{
+    Console.WriteLine($"No!"):
+}
